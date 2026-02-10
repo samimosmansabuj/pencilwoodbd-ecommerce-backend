86e4b6b9af944b5f7a4b205c1c6feee87b83592b
SumiyaAkter701 <sumiyaakter.9586@gmail.com>
2026-02-10 16:08:50 +0600

    Create api_views2.py for Tisshue box landing page. and work on it.

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .api_views import CategoryAPIViews, ProductViews
-
+from .api_views2 import *
 app_name = "api"
 
 
@@ -8,4 +8,8 @@ urlpatterns = [
     
     path("api/category/", CategoryAPIViews.as_view(), name="category_api"),
     path("api/product/", ProductViews.as_view(), name="product_api"),
+# for tissue-box-landing page
+    path("api2/categories/", CategoryAPIViews2.as_view(), name="categories-api-2"),
+    path("api2/landing/tissue-box/products/",TissueBoxLandingProductViews.as_view(),name="tissue-box-products-2"),
+    path("api2/landing/tissue-box/order/",TissueBoxLandingOrderAPI.as_view(),name="tissue-box-order-2"),
 ]
