82fe2f8ed20bdd7d0e89d3e8b64429d78c748d1b
Samim Osman Sabuj <samim.o.sabuj01@gmail.com>
2026-03-08 05:24:00 +0600

    static update!!!

--- pencilwoodbd/settings.py
+++ pencilwoodbd/settings.py
@@ -201,7 +201,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static', ]
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 # STATIC_ROOT = BASE_DIR / 'collectstatic'
