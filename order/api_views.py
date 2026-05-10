340f7f92d81d4983a8658adf387af9fa3f8c02b9
SumiyaAkter701 <sumiyaakter.9586@gmail.com>
2026-05-10 19:37:38 +0600

    order api_views fix/modify.

--- order/api_views.py
+++ order/api_views.py
@@ -84,12 +84,12 @@ class CheckoutSummaryAPIView(APIView):
 
             if not cart_items.exists():
                 return Response(
-                    {"status": False, "message": "Cart is empty"},
+                    {"status": False, "message": "Cart empty"},
                     status=400
                 )
 
             items_data = []
-            total = Decimal("0")
+            subtotal = Decimal("0")
 
             for item in cart_items:
                 items_data.append({
@@ -98,18 +98,18 @@ class CheckoutSummaryAPIView(APIView):
                     "price": item.price,
                     "total": item.total_price
                 })
-                total += item.total_price
 
-            # simple delivery logic (you can upgrade later)
+                subtotal += item.total_price
+
             delivery_charge = Decimal("60")
 
             return Response({
                 "status": True,
                 "data": {
                     "items": items_data,
-                    "subtotal": total,
+                    "subtotal": subtotal,
                     "delivery_charge": delivery_charge,
-                    "grand_total": total + delivery_charge
+                    "grand_total": subtotal + delivery_charge
                 }
             })
 
@@ -118,7 +118,7 @@ class CheckoutSummaryAPIView(APIView):
                 {"status": False, "message": str(e)},
                 status=500
             )
-        
+
 
 class PlaceOrderAPIView(APIView):
     permission_classes = [IsAuthenticated]
@@ -128,11 +128,15 @@ class PlaceOrderAPIView(APIView):
             with transaction.atomic():
 
                 customer = request.user.customer_profile
-                cart_items = AddToCart.objects.filter(customer=customer)
+
+                cart_items = AddToCart.objects.select_related(
+                    "product",
+                    "variant"
+                ).filter(customer=customer)
 
                 if not cart_items.exists():
                     return Response(
-                        {"status": False, "message": "Cart is empty"},
+                        {"status": False, "message": "Cart empty"},
                         status=400
                     )
 
@@ -140,9 +144,12 @@ class PlaceOrderAPIView(APIView):
                 district = request.data.get("district")
                 upazila = request.data.get("upazila")
 
-                if not address_text:
+                if not address_text or not district:
                     return Response(
-                        {"status": False, "message": "Address required"},
+                        {
+                            "status": False,
+                            "message": "Address & district required"
+                        },
                         status=400
                     )
 
@@ -165,25 +172,37 @@ class PlaceOrderAPIView(APIView):
                     product = item.product
                     variant = item.variant
 
-                    # stock validation
+                    # STOCK CHECK
                     if variant:
+
                         if variant.inventory_quantity < item.quantity:
                             return Response(
-                                {"status": False, "message": f"{product.name} out of stock"},
+                                {
+                                    "status": False,
+                                    "message": f"{product.name} out of stock"
+                                },
                                 status=400
                             )
+
                         variant.inventory_quantity -= item.quantity
                         variant.save()
+
                         price = variant.discount_price or variant.price
 
                     else:
+
                         if product.inventory_quantity < item.quantity:
                             return Response(
-                                {"status": False, "message": f"{product.name} out of stock"},
+                                {
+                                    "status": False,
+                                    "message": f"{product.name} out of stock"
+                                },
                                 status=400
                             )
+
                         product.inventory_quantity -= item.quantity
                         product.save()
+
                         price = product.discount_price or product.price
 
                     OrderItem.objects.create(
@@ -191,12 +210,13 @@ class PlaceOrderAPIView(APIView):
                         product=product,
                         variant=variant,
                         quantity=item.quantity,
-                        price=price
+                        price=price,
+                        discount_price=price,
+                        discount_total_price=price * item.quantity
                     )
 
                     total += price * item.quantity
 
-                # delivery
                 delivery_charge = Decimal("60")
 
                 order.total_cost = total + delivery_charge
@@ -217,6 +237,227 @@ class PlaceOrderAPIView(APIView):
                 {"status": False, "message": str(e)},
                 status=500
             )
+
+
+class OrderListAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        customer = getattr(request.user, "customer_profile", None)
+
+        orders = Order.objects.filter(
+            customer=customer
+        ).order_by("-id")
+
+        return Response({
+            "status": True,
+            "data": [
+                {
+                    "order_id": o.order_id,
+                    "total": o.total_cost,
+                    "status": o.status,
+                    "payment_status": o.payment_status
+                }
+                for o in orders
+            ]
+        })
+
+
+class OrderDetailAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, order_id):
+        try:
+            customer = getattr(request.user, "customer_profile", None)
+
+            if not customer:
+                return Response(
+                    {
+                        "status": False,
+                        "message": "Customer not found"
+                    },
+                    status=404
+                )
+
+            order = get_object_or_404(
+                Order,
+                order_id=order_id,
+                customer=customer
+            )
+
+            data = {
+                "order_id": order.order_id,
+                "status": order.status,
+                "payment_status": order.payment_status,
+                "total": order.total_cost,
+                "delivery": order.shipping_total,
+                "address": order.shipping_address,
+                "date": order.created_at,
+                "items": [
+                    {
+                        "product": i.product.name,
+                        "variant": i.variant.attributes if i.variant else None,
+                        "quantity": i.quantity,
+                        "price": i.price,
+                        "total": i.discount_total_price
+                    }
+                    for i in order.order_items.all()
+                ]
+            }
+
+            return Response({
+                "status": True,
+                "data": data
+            })
+
+        except Exception as e:
+            return Response(
+                {"status": False, "message": str(e)},
+                status=500
+            )
+
+
+class PaymentMethodListAPIView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        methods = PaymentMethod.objects.all()
+
+        return Response({
+            "status": True,
+            "data": [
+                {
+                    "id": m.id,
+                    "name": m.payment_option,
+                    "account_number": m.account_number,
+                    "account_name": m.account_name
+                }
+                for m in methods
+            ]
+        })
+
+
+class CreatePaymentAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            customer = request.user.customer_profile
+
+            order_id = request.data.get("order_id")
+            payment_method_id = request.data.get("payment_method_id")
+            transaction_id = request.data.get("transaction_id")
+
+            order = Order.objects.filter(
+                order_id=order_id,
+                customer=customer
+            ).first()
+
+            if not order:
+                return Response(
+                    {"status": False, "message": "Order not found"},
+                    status=404
+                )
+
+            if order.payment_status == PAYMENT_STATUS.Paid:
+                return Response(
+                    {"status": False, "message": "Already paid"},
+                    status=400
+                )
+
+            payment_method = PaymentMethod.objects.filter(
+                id=payment_method_id
+            ).first()
+
+            if not payment_method:
+                return Response(
+                    {"status": False, "message": "Invalid payment method"},
+                    status=400
+                )
+
+            payment = Payment.objects.create(
+                order=order,
+                payment_method=payment_method,
+                provider=payment_method.payment_option,
+                amount=order.total_cost,
+                transaction_id=transaction_id,
+                status="pending"
+            )
+
+            order.payment_type = PAYMENT_TYPE.ONLINE
+            order.payment_status = PAYMENT_STATUS.Pending
+            order.save()
+
+            return Response({
+                "status": True,
+                "message": "Payment submitted",
+                "payment_id": payment.id
+            })
+
+        except Exception as e:
+            return Response(
+                {"status": False, "message": str(e)},
+                status=500
+            )
+
+
+class VerifyPaymentAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+
+        if request.user.user_type not in [
+            USER_TYPE.ADMIN,
+            USER_TYPE.SUPER_ADMIN
+        ]:
+            return Response(
+                {"status": False, "message": "Permission denied"},
+                status=403
+            )
+
+        try:
+            payment_id = request.data.get("payment_id")
+            status_value = request.data.get("status")
+
+            if status_value not in ["success", "failed"]:
+                return Response(
+                    {"status": False, "message": "Invalid status"},
+                    status=400
+                )
+
+            payment = Payment.objects.filter(
+                id=payment_id
+            ).select_related("order").first()
+
+            if not payment:
+                return Response(
+                    {"status": False, "message": "Payment not found"},
+                    status=404
+                )
+
+            payment.status = status_value
+            payment.save()
+
+            if status_value == "success":
+                payment.order.payment_status = PAYMENT_STATUS.Paid
+            else:
+                payment.order.payment_status = PAYMENT_STATUS.Failed
+
+            payment.order.save()
+
+            return Response({
+                "status": True,
+                "message": "Payment verified"
+            })
+
+        except Exception as e:
+            return Response(
+                {"status": False, "message": str(e)},
+                status=500
+            )
+
+
+
 
 
 # class EcomOrderCreateAPIView(APIView):
@@ -290,200 +531,3 @@ class PlaceOrderAPIView(APIView):
 
 #         except Exception as e:
 #             return Response({"status": False, "message": str(e)}, status=400)
-
-
-class OrderListAPIView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        customer = getattr(request.user, "customer_profile", None)
-
-        orders = Order.objects.filter(customer=customer)
-
-        return Response({
-            "status": True,
-            "data": [
-                {
-                    "order_id": o.order_id,
-                    "total": o.total_cost,
-                    "status": o.status
-                } for o in orders
-            ]
-        })
-
-
-class OrderDetailAPIView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request, order_id):
-        try:
-            customer = getattr(request.user, "customer_profile", None)
-
-            if not customer:
-                return Response(
-                    {"status": False, "message": "Customer not found"},
-                    status=404
-                )
-
-            order = get_object_or_404(
-                Order,
-                order_id=order_id,
-                customer=customer
-            )
-
-            data = {
-                "order_id": order.order_id,
-                "status": order.status,
-                "payment_status": order.payment_status,
-                "total": order.total_cost,
-                "delivery": order.shipping_total,
-                "address": order.shipping_address,
-                "date": order.created_at,
-                "items": [
-                    {
-                        "product": i.product.name,
-                        "variant": i.variant.attributes if i.variant else None,
-                        "quantity": i.quantity,
-                        "price": i.price,
-                        "total": i.discount_total_price
-                    }
-                    for i in order.order_items.all()
-                ]
-            }
-
-            return Response({"status": True, "data": data})
-
-        except Exception as e:
-            return Response(
-                {"status": False, "message": str(e)},
-                status=500
-            )
-        
-
-class PaymentMethodListAPIView(APIView):
-    permission_classes = [AllowAny]
-
-    def get(self, request):
-        methods = PaymentMethod.objects.all()
-
-        return Response({
-            "status": True,
-            "data": [
-                {
-                    "id": m.id,
-                    "name": m.payment_option,
-                    "account_number": m.account_number,
-                    "account_name": m.account_name
-                }
-                for m in methods
-            ]
-        })
-    
-
-class CreatePaymentAPIView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request):
-        try:
-            customer = request.user.customer_profile
-
-            order_id = request.data.get("order_id")
-            payment_method_id = request.data.get("payment_method_id")
-            transaction_id = request.data.get("transaction_id")
-
-            order = Order.objects.filter(order_id=order_id, customer=customer).first()
-
-            if not order:
-                return Response(
-                    {"status": False, "message": "Order not found"},
-                    status=404
-                )
-
-            if order.payment_status == PAYMENT_STATUS.Paid:
-                return Response(
-                    {"status": False, "message": "Already paid"},
-                    status=400
-                )
-
-            payment_method = PaymentMethod.objects.filter(id=payment_method_id).first()
-
-            if not payment_method:
-                return Response(
-                    {"status": False, "message": "Invalid payment method"},
-                    status=400
-                )
-
-            payment = Payment.objects.create(
-                order=order,
-                payment_method=payment_method,
-                provider=payment_method.payment_option,
-                amount=order.total_cost,
-                transaction_id=transaction_id,
-                status="pending"
-            )
-
-            # update order
-            order.payment_type = PAYMENT_TYPE.ONLINE
-            order.payment_status = PAYMENT_STATUS.Pending
-            order.save()
-
-            return Response({
-                "status": True,
-                "message": "Payment submitted",
-                "payment_id": payment.id
-            })
-
-        except Exception as e:
-            return Response(
-                {"status": False, "message": str(e)},
-                status=500
-            )
-        
-class VerifyPaymentAPIView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request):
-        try:
-            if request.user.user_type not in [USER_TYPE.ADMIN, USER_TYPE.SUPER_ADMIN]:
-                return Response(
-                    {"status": False, "message": "Permission denied"},
-                    status=403
-                )
-
-            payment_id = request.data.get("payment_id")
-            status_value = request.data.get("status")
-
-            if status_value not in ["success", "failed"]:
-                return Response(
-                    {"status": False, "message": "Invalid status"},
-                    status=400
-                )
-
-            payment = Payment.objects.filter(id=payment_id).select_related("order").first()
-
-            if not payment:
-                return Response(
-                    {"status": False, "message": "Payment not found"},
-                    status=404
-                )
-
-            payment.status = status_value
-            payment.save()
-
-            if status_value == "success":
-                payment.order.payment_status = PAYMENT_STATUS.Paid
-            else:
-                payment.order.payment_status = PAYMENT_STATUS.Failed
-
-            payment.order.save()
-
-            return Response({
-                "status": True,
-                "message": "Payment verified"
-            })
-
-        except Exception as e:
-            return Response(
-                {"status": False, "message": str(e)},
-                status=500
-            )
